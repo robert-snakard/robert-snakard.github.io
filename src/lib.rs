a0ca0747f632bb7556c28c49e37fcd36dfc91bdd
Robert Snakard <robert.snakard@gmail.com>
2019-10-24 23:06:08 -0700

    First Wasm!

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[no_mangle]
+pub fn two_plus(x: u8) -> u8 {
+    return x + 2;
+}
+
+#[test]
+fn it_works() {
+    let x = two_plus(2);
+    assert_eq!(x, 4);
 }
